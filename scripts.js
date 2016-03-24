f29bb30f36483b60e616b3a2124718303a755dab
PhilipRurka <DefFreeman@Philips-MacBook-Pro.local>
2016-03-24 12:03:41 -0300

    First JavaScript Project

--- scripts.js
+++ scripts.js
@@ -1,5 +1,16 @@
 console.log("Program Begin");
-alert('I have created a basic program that will figure out what your number is. What is going to happen is after your 5 digit number has been inserted, I will then generate a random 4 digit number, you will then respond with a 4 digit number and I will do the same and so on. We will repete this prosses until we have 5 rows. Follow the instructions.')
-var prediction = ('Please insert a 5 digit number. I also recomend writing this number down.')
-
+alert('Please follow the instructions.');
+var prediction = 27571;
+var first_number = parseInt(prediction) - 20002;
+document.write(first_number);
+var input = prompt('Please insert any 4 digit number.');
+document.write('<br>' + input);
+var calc = 9999 - input;
+document.write('<br>'+ calc);
+input = prompt('please insert any 4 digit number.');
+document.write('<br>' + input);
+calc =9999 - input;
+document.write('<br>' + calc);
+alert('If we add all of these up, we get the number at the very top!')
+document.write('<br>' + '<br>' + 27571)
 console.log("Program Complete");

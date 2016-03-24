b5508e38253d56d5a391f2fc4274c9e7330723a2
PhilipRurka <DefFreeman@Philips-MacBook-Pro.local>
2016-03-24 10:28:51 -0300

    Why did it change the lay out?

--- scripts.js
+++ scripts.js
@@ -1,2 +1,5 @@
 console.log("Program Begin");
+alert('I have created a basic program that will figure out what your number is. What is going to happen is after your 5 digit number has been inserted, I will then generate a random 4 digit number, you will then respond with a 4 digit number and I will do the same and so on. We will repete this prosses until we have 5 rows. Follow the instructions.')
+var prediction = ('Please insert a 5 digit number. I also recomend writing this number down.')
+
 console.log("Program Complete");

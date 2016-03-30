09cdd784d5a3152eccfc157824c21239de9296a2
PhilipRurka <DefFreeman@Philips-MBP.Home>
2016-03-29 21:41:14 -0300

    New JS project

--- scripts.js
+++ scripts.js
@@ -1,39 +1,43 @@
 console.log("Program Begin");
 
-alert('Please respond yes if you see your number.')
+var ac_name = prompt('What is my name?');
+var pr_name = prompt('What is my prefered name?');
+var place = prompt('Local favourite place?');
+var fav_number = prompt('What is my favourite number?');
+var age = prompt('What is my age?');
+var score = 0;
 
-var first_block = prompt('2  3  6  7  10  11  14  15  18  19  22  23  26  27  30  31  34  35  38  39  42  43  46  47  50  51  54  55  58  59  62  63');
-var second_block = prompt('16  17  18  19  20  21  22  23  24  25  26  27  28  29  30  31  48  49  50  51  52  53  54  55  56  57  58  59  60  61  62  63');
-var third_block = prompt('8  9  10  11  12  13  14  15  24  25  26  27  28  29  30  31  40  41  42  43  44  45  46  47  56  57  58  59  60  61  62  63');
-var fourth_block = prompt('1  3  5  7  9  11  13  15  17  19  21  23  25  27  29  31  33  35  37  39  41  43  45  47  49  51  53  55  57  59  61  63');
-var fifth_block = prompt('4  5  6  7  12  13  14  15  20  21  22  23  28  29  30  31  36  37  38  39  44  45  46  47  52  53  54  55  60  61  62  63');
-var sixth_block = prompt('32  33  34  35  36  37  38  39  40  41  42  43  44  45  46  47  48  49  50  51  52  53  54  55  56  57  58  59  60  61  62  63');
-var answer = 0;
-
-if (first_block.toUpperCase() === 'YES') {
-	answer = 2;
-} 
-
-if (second_block.toUpperCase() === 'YES') {
-	answer += 16;
+if (ac_name.toUpperCase() === 'PHILIP') {
+	score += + 1;
 }
 
-if (third_block.toUpperCase() === 'YES') {
-	answer += 8;
+if (pr_name.toUpperCase() === 'DEF') {
+	score += + 1;
 }
 
-if (fourth_block.toUpperCase() === 'YES') {
-	answer += 1
+if (place.toUpperCase() === 'TEMPLE OF TIME') {
+	score += + 1;
+}
+if (fav_number === '23') {
+	score += + 1;
+}
+if (age === '28') {
+	score += + 1;
 }
 
-if (fifth_block.toUpperCase() === 'YES') {
-	answer += 4;
+if (score === 5) {
+	alert('You know me very well! You got them all right!');
+} else if (score === 4) {
+	alert('You know me pretty well. You got all but one right.');
+} else if (score === 3) {
+	alert('You don\'t seem to know me very well. You got 3 right.');
+} else if ( score === 2) {
+	alert('Do you even know me? You got 2 out of 5 right.')
+} else if (score === 1) {
+	alert('Do I even know you? You got 1 out of 5 right! Come on!');
+} else {
+	alert('... you suck.');
 }
 
-if (sixth_block.toUpperCase() === 'YES') {
-	answer += 32;
-}
-
-document.write('<p>The number you picked was ' + answer + '.</p>');
 
 console.log("Program Complete");

2e31de4495987b9088e9532438ed2558074faa11
PhilipRurka <DefFreeman@Philips-MBP.Home>
2016-04-05 10:46:09 -0300

    Update

---
+++ Meeting.js
@@ -0,0 +1,9 @@
+function red_black() {
+
+	if ( document.getElementById("first").style.color == "red" ) {
+		document.getElementById("first").style.color = "#fff";
+	} else {
+		document.getElementById("first").style.color = "red";
+	}
+
+}

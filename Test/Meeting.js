eac7f0b46383b1485c79a9542588bb55eae7c75f
PhilipRurka <DefFreeman@Philips-MacBook-Pro.local>
2016-04-07 12:28:38 -0300

    Finish challenge

--- Test/Meeting.js
+++ Test/Meeting.js
@@ -1,13 +1,14 @@
 
 var student;
 var message = '';
+var input;
 
-for (i = 0; i < students.length; i += 1) {
-	student = students[i];
-	message += '<h2>Student: ' + student.name + '</h2>';
-	message += '<p>Track: ' + student.track + '</p>';
-	message += '<p>Achievements: ' + student.achievements + '</p>';
-	message += '<p>Points: ' + student.points + '</p>';
+function student_info (student) {
+	var info= '<h2>Student: ' + student.name + '</h2>';
+	info += '<p>Track: ' + student.track + '</p>';
+	info += '<p>Achievements: ' + student.achievements + '</p>';
+	info += '<p>Points: ' + student.points + '</p>';
+	return info;
 }
 
 function print(message) {
@@ -15,4 +16,18 @@ function print(message) {
 	output_div.innerHTML = message;
 }
 
-print(message);
+while (true) {
+	input = prompt('Please enter the name you want to look up. You can type [List] to view who\'s name is on file. If you wish to quite this loop, simply type in [quite].');
+	if (input === null || input.toLowerCase() === 'quite') {
+		break;
+	} else if (input.toLowerCase() === 'list') {
+		input = prompt('Please enter the name you want to look up. The names found on this file are [Dave], [Jody], [Jordan], [John] and [Trish]. If you wish to quite this loop, simply type in [quite].');
+	}
+		for (i = 0; i < students.length; i += 1) {
+			student = students[i];
+			if(input.toLowerCase() === student.name.toLowerCase()) {
+				message = student_info(student);
+				print(message);
+			}
+	}
+}

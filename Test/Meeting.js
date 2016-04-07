7733eb9a3a43a4ab234c0816269cd5264fbeb0e4
PhilipRurka <DefFreeman@Philips-MacBook-Pro.local>
2016-04-07 11:40:00 -0300

    New project.
    Needed to cheat but I understand a bit more.

--- Test/Meeting.js
+++ Test/Meeting.js
@@ -1,92 +1,18 @@
-var counter = 0;
-var correct_ans = 0;
-var question;
-var answer;
-var response;
-var html;
-var correct_list = [];
-var wrong_list = [];
-var wrong;
 
-var questions = [
-	{
-		question : 'What is my favourite number?',
-		answer : 23
-	},
-	{
-		question : 'Number one event I want to attend?',
-		answer : 'burning man'
-	},
-	{
-		question : 'What is my favourite food?',
-		answer : 'sushi'
-	},
-	{
-		question : 'What is my favourtite language?',
-		answer : 'math'
-	},
-	{
-		question : 'Coke or Pepsi?',
-		answer : 'neither'
-	}
-];
+var student;
+var message = '';
 
-
-function red_black_1() {
-	if ( document.getElementById("first").style.color == "red" ) {
-		document.getElementById("first").style.color = "#000";
-	} else {
-		document.getElementById("first").style.color = "red";
-		if (counter < 4) {
-		counter += 1;
-		} else {
-			questions_func();
-			html = 'You got ' + correct_ans + ' out of ' + questions.length + ' question(s) right';
-			html += '<h2> You got these questions right:';
-			html += build_list( correct_list );
-			html += '<h4>You got these questions wrong:</h4>';
-			html += build_list( wrong_list );
-			document.write(html);
-		}
-	}
+for (i = 0; i < students.length; i += 1) {
+	student = students[i];
+	message += '<h2>Student: ' + student.name + '</h2>';
+	message += '<p>Track: ' + student.track + '</p>';
+	message += '<p>Achievements: ' + student.achievements + '</p>';
+	message += '<p>Points: ' + student.points + '</p>';
 }
 
-function questions_func() {
-	for (var i = 0; i < questions.length; i += 1) {
-		question = questions [i].question;
-		answer = questions [i].answer;
-		response = prompt(question);
-		if (response.toLowerCase() === answer || parseInt(response) === answer){
-			correct_ans += 1;
-			correct_list.push(question);
-		} else {
-			wrong_list.push(question);
-		}
-	}
+function print(message) {
+	var output_div = document.getElementById('output');
+	output_div.innerHTML = message;
 }
 
-function build_list (arr) {
-	var list_html = '<ol>';
-	for (a = 0; a < arr.length; a+=1) {
-		list_html += '<li>' + arr[a] + '</li>';
-	}
-	list_html += '</ol>';
-	return list_html;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(message);

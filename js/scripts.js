bc0bb037223d428c0b22a481de5d6424fdb1b0f0
PhilipRurka <DefFreeman@Philips-MacBook-Pro.local>
2016-04-18 10:15:06 -0300

    On the right track!

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
-$('#menu').click(function (){
-	$('#menu').hide('slow');
-//small delay
-	
-//make it fade to black
-	$('#menu_click').addClass('BG');
-//button's and text appears
-	$('#menu').removeClass('menu_logo').addClass('big_x').show('slow');
+var $overlay = $('<div id="overlay"></div>')
+
+$('body').append($overlay);
+
+$('#menu').click(function(event) {
+	event.preventDefault();
+	$(this).hide(200);
+	$(this).removeClass('menu_logo').addClass('big_x').show(200);
+	$overlay.show('slow');
 });

39fdb8ca144c06ee3f113f0b15df7880b596390b
PhilipRurka <DefFreeman@Philips-MacBook-Pro.local>
2016-04-18 12:24:04 -0300

    Overlap Menu Done
    Need to fix a few little things.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
-var $overlay = $('<div id="overlay"></div>')
+var $overlay = $('<div id="overlay"><ul><li><a href="lab.html">Lab</a></li><li><a href="home_gallery.html">Home Gallery</a></li><li><a href="eco.html">Eco</a></li><li><a href="contact.html">Contact</a></li></ul></div>')
 
 $('body').append($overlay);
+
 
 $('#menu').click(function(event) {
 	event.preventDefault();

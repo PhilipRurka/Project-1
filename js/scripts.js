b8a48e1e7027a31f2743ce1a8e3c54eb59bda59e
PhilipRurka <DefFreeman@Philips-MacBook-Pro.local>
2016-04-18 12:36:36 -0300

    Done index.
    Need to fix a few things.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 var $overlay = $('<div id="overlay"><ul><li><a href="lab.html">Lab</a></li><li><a href="home_gallery.html">Home Gallery</a></li><li><a href="eco.html">Eco</a></li><li><a href="contact.html">Contact</a></li></ul></div>')
+var swich = 0;
 
 $('body').append($overlay);
 
 
 $('#menu').click(function(event) {
 	event.preventDefault();
+	if (swich === 0) {
 	$(this).hide(200);
 	$(this).removeClass('menu_logo').addClass('big_x').show(200);
 	$overlay.show('slow');
-});
+swich += 1;
+} else {
+	$(this).hide(200);
+	$(this).removeClass('big_x').addClass('menu_logo').show(200);
+	$overlay.hide('slow');
+	swich = swich - 1;
+};
+})

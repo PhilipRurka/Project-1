542e6fcfeed117378fbda5b3ca305e30229a1f7f
PhilipRurka <DefFreeman@Philips-MacBook-Pro.local>
2016-04-20 21:47:36 -0300

    Added Pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 var $overlay = $('<div id="overlay"></div>');
-var $ul = $('<ul></ul>')
-var $lab = $('<li><a href="lab.html">Lab</a></li>');
-var $gallery = $('<li><a href="home_gallery.html">Home Gallery</a></li>');
-var $eco = $('<li><a href="eco.html">Eco</a></li>');
-var $contact = $('<li><a href="contact.html">Contact</a></li>');
+var $ul = $('<ul></ul>');
+var $lab = $('<li><a href="pages/lab.html">Lab</a></li>');
+var $gallery = $('<li><a href="pages/home_gallery.html">Home Gallery</a></li>');
+var $eco = $('<li><a href="pages/eco.html">Eco</a></li>');
+var $contact = $('<li><a href="pages/contact.html">Contact</a></li>');
 var swich = 0;
 
 $overlay.append($ul);
@@ -24,4 +24,4 @@ swich += 1;
 	$overlay.hide('slow');
 	swich = swich - 1;
 };
-})
+});

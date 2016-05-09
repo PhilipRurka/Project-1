6ce7b94674b4405021e08286b4e15fd03fd2d2b9
PhilipRurka <DefFreeman@Philips-MacBook-Pro.local>
2016-05-09 15:11:18 -0400

    Fade overlay and organized sass

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var $overlay = $('<div id="overlay"></div>');
 var $ul = $('<ul></ul>');
 var $lab = $('<li><a href="pages/lab.html">Lab</a></li>');
-var $gallery = $('<li><a href="pages/home_gallery.html">Home Gallery</a></li>');
+var $gallery = $('<li><a href="pages/gallery.html">Home Gallery</a></li>');
 var $eco = $('<li><a href="pages/eco.html">Eco</a></li>');
 var $contact = $('<li><a href="pages/contact.html">Contact</a></li>');
 var swich = 0;
@@ -14,14 +14,14 @@ $('#overlay').hide();
 $('#menu').click(function(event) {
 	event.preventDefault();
 	if (swich === 0) {
-	$(this).hide(200);
-	$(this).removeClass('menu_logo').addClass('big_x').show(200);
-	$overlay.show('slow');
+	$(this).hide();
+	$(this).removeClass('menu_logo').addClass('big_x').show();
+	$overlay.fadeIn(200);
 swich += 1;
 } else {
-	$(this).hide(200);
-	$(this).removeClass('big_x').addClass('menu_logo').show(200);
-	$overlay.hide('slow');
+	$(this).hide();
+	$(this).removeClass('big_x').addClass('menu_logo').show();
+	$overlay.fadeOut(200);
 	swich = swich - 1;
 };
 });
